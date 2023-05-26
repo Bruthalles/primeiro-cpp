f0479404ef4c55a5e2706d040d44a68548ca91d7
Bruthalles <128196904+Bruthalles@users.noreply.github.com>
2023-05-26 12:45:01 -0300

    Add files via upload

---
+++ trab_program_cmp 1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   cout<< "Exercicio 1 "<<endl;
+	float notaA , notaB , media;
+	cout<< " Digite primeira nota"<<endl;
+	cin>> notaA;
+	cout<< "  Digite segunda nota"<<endl;
+	cin>> notaB;
+	media =(notaA + notaB)/2;
+	cout<< "Sua media abaixo"<<endl;
+	cout<< media<<endl;
+	
+	
+	
+{  cout<< "Exercicio 2 "<<endl;
+   float a , b , x , y;
+   cout<< "Vamos montar uma formula ! "<<endl;
+   cout<< " Usaremos as seguintes variaveis: a, b, x, y "<<endl;
+   cout<< " Formula: x = (2+a)/(b+4)-2y "<<endl<<endl;
+   cout<< " 1- Descreva um valor para a variavel a "<<endl;
+   cin>> a;
+   cout<< " 2- Descreva um valor para a variavel b "<<endl;
+   cin>> b;
+   cout<< " 3- Descreva um valor para a variavel y "<<endl;
+   cin>> y;	
+   x = (2+a)/(b+4)- 2*y;
+   cout<< "Resultado da formula sera:"<<endl;
+   cout<< x <<endl;
+   
+   
+{   cout<< "Exercicio 3 "<<endl;
+	float x , y , b , k;
+	cout<< "Vamos para mais uma formula "<<endl;
+	cout<< "y = (2b + 5+3k) /2x"<<endl;
+	cout<< "1- Digite o valor de b"<<endl;
+	cin>> b;
+	cout<< "2- Digite o valor de k"<<endl;
+	cin>> k;
+	cout<< "3- Digite o valor de x"<<endl;
+	cin>> x;
+	y = (2*b + 5 + 3*k)/2*x;
+	cout<< "Resultado:"<<endl;
+	cout<< y <<endl;
+	
+	{   cout<< " Exercicio 4 "<<endl;
+	float D , x , y;
+	cout<< " Terceira formula "<<endl;
+	cout<< " D = (x2 – x1)2 + (y2 – y1)2"<<endl;
+	cout<< " Digite valor de x "<<endl;
+	cin>> x;
+	cout<< " Digite valor de y "<<endl;
+	cin>> y;
+    D = (x*2-x*1)*2 + (y*2-y*1)*2;
+    cout<< "Resultado final de D: "<<endl;
+    cout<< D <<endl;
+    
+    
+    {  cout<< "Exercicio 5 "<<endl;
+	float A , B;
+	cout<< "Digite valor de A "<<endl;
+	cin>> A;
+	cout<< "Digite valor de B "<<endl;
+	cin>> B;
+	
+if (A>B)
+{
+cout<< " Se A maior que B entao B esta contido em A"<<endl;
+}
+else 
+	if (B>A)
+	{
+		cout<< " Se B maior que A entao A esta contido em B "<<endl;
+	}
+	else
+	    if (A=B)
+		    cout<< " Se A igual a B entao B esta contido em A e A esta contido em B "<<endl;
+	
+
+}	
+	
+}
+}	
+}	
+
+cout<< "Grupo: Thalles Roberto"<<endl;
+cout<< "Gabriel Grutti"<<endl;
+cout<< "Guilherme Agra"<<endl; 
+}
+       
